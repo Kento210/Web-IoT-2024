038252e451aa8367444d980218041ab8cac327a9
Kento210 <kentokentokento913@gmail.com>
2024-02-23 05:53:00 +0900

    fix: entrypoint.shのタイポを修正

--- nozawana/entrypoint.sh
+++ nozawana/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # rm ではrailsのpidを削除
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
-rm -f /app/tmp/pids/server.pid
+rm -f /nozawana/tmp/pids/server.pid
 
 # exec "$@"でCMDで渡されたコマンドを実行しています。(→rails s)
 # コンテナのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
