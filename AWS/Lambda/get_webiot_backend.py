1c7b506fbca4fc0c9bfdfa0e250035143f47578e
Kento210 <kentokentokento913@gmail.com>
2024-03-03 04:53:00 +0900

    update: Decimal型を変換できるように更新

--- AWS/Lambda/get_webiot_backend.py
+++ AWS/Lambda/get_webiot_backend.py
@@ -1,26 +1,36 @@
 import json
 import boto3
+from decimal import Decimal
 
-# DynamoDBリソースを初期化
-dynamodb = boto3.resource('dynamodb')
-# DynamoDBテーブル名を指定
-table_name = 'ButtonPressEventTable'
-table = dynamodb.Table(table_name)
+# Decimal型をfloat型に変換するカスタムエンコーダ
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        # 他の型の場合は、スーパークラスのメソッドを呼び出す
+        return super(DecimalEncoder, self).default(obj)
 
 def lambda_handler(event, context):
-    # DynamoDBテーブルから全項目を取得
+    dynamodb = boto3.resource('dynamodb')
+    table_name = 'YourTableName'
+    table = dynamodb.Table(table_name)
+    
     try:
         response = table.scan()
-        items = response['Items']
+        # Decimal型のデータを含むレスポンスをJSONに変換
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Items'], cls=DecimalEncoder),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
-            'body': json.dumps('Error retrieving data from DynamoDB')
+            'body': json.dumps('Error fetching data from DynamoDB'),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
-
-    # 取得したデータをレスポンスとして返す
-    return {
-        'statusCode': 200,
-        'body': json.dumps(items)
-    }
